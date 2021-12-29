f897324b00163872ba8621301cca97187f8f0040
d3val <devalt5008@gmail.com>
2021-12-28 19:28:19 -0600

    Data persistance
    - Player's name persists between scenes
    - Max score is saved with his player's name in savefile.json
    - (error) After game over and pressing space, max score text doesn't update.

---
+++ Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,91 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ScoreManager : MonoBehaviour
+{
+    public static ScoreManager instance;
+    public static string playerName = "";
+    public static int sessionScore = 0;
+
+    public static string maxScorePlayerName;
+    public static int maxScore;
+
+    void Awake()
+    {
+        if (instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        instance = this;
+        DontDestroyOnLoad(gameObject);
+        LoadMaxScore();
+
+        if (maxScore == 0)
+        {
+            maxScorePlayerName = "";
+        }
+    }
+
+    [System.Serializable]
+    class SaveData
+    {
+        public int maxScore;
+        public string name;
+    }
+
+    public void LoadMainScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void ReturnToMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void Quit()
+    {
+#if UNITY_EDITOR
+        EditorApplication.ExitPlaymode();
+#else
+        Application.Quit();
+#endif
+    }
+
+    public static void SaveMaxScore()
+    {
+        SaveData data = new SaveData();
+        data.name = playerName;
+        data.maxScore = sessionScore;
+
+        string json = JsonUtility.ToJson(data);
+
+        File.WriteAllText(Application.persistentDataPath + "/savefile.json", json);
+    }
+
+    public void LoadMaxScore()
+    {
+        string path = Application.persistentDataPath + "/savefile.json";
+        if (File.Exists(path))
+        {
+            string json = File.ReadAllText(path);
+            SaveData data = JsonUtility.FromJson<SaveData>(json);
+
+            maxScore = data.maxScore;
+            maxScorePlayerName = data.name;
+        }
+    }
+
+    public void ReadName()
+    {
+        Text nameField = GameObject.Find("IF Name").transform.GetChild(2).GetComponent<Text>();
+        playerName = nameField.text;
+    }
+}
